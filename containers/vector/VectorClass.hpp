8fb1ea18c5ab169f671cef46d09319af59f8427f
malatini <malatini@student.42.fr>
2021-11-02 11:20:35 +0100

    save

---
+++ containers/vector/VectorClass.hpp
@@ -0,0 +1,10 @@
+#ifndef VECTORCLASS_HPP
+# define VECTORCLASS_HPP
+
+/* Le vrai vecteur est defini dans <vector> */
+#include <memory>
+
+template <class T, class Allocator = std::allocator<T>>
+class vector;
+
+#endif
