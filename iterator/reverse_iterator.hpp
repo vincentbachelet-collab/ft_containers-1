ee22a9efe571527fda8ea3ed8d591de95b354630
malatini <malatini@student.42.fr>
2021-11-08 18:13:47 +0100

    reverse iterator

---
+++ iterator/reverse_iterator.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+//a reprendre
+namespace ft
+{
+    template <typename Iterator>
+    class reverse_iterator
+    {
+        typedef Iterator                                iterator_type;
+        typedef typename Iterrator::iterator_category   iterator_category;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::difference_type      difference_type;
+    };
+}
