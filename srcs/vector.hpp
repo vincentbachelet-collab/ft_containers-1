bac3c0721c12fa4ac4cb3ea9dc737973c8b66cab
malatini <malatini@student.42.fr>
2021-11-10 09:50:11 +0100

    save

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -12,7 +12,7 @@
 #include <vector>
 #include <iterator>
 //#include "../srcs/utils.hpp"
-//#include "../srcs/vector_iterator.hpp"
+#include "../srcs/vector_iterator.hpp"
 //#include "../srcs/reverse_iterator.hpp"
 
 # define DEBUG 1
@@ -31,25 +31,34 @@ namespace ft
 		typedef value_type*													pointer;
 		typedef const value_type*											const_pointer;
 	
-		//Mettre ft au lieux de std
-		//typedef std::vector_iterator<value_type>		iterator;
-		//typedef std::vector<value_type>::iterator<value_type>				iterator;
 
-		//typedef std::vector_iterator<const value_type>	const_iterator;
-		typedef std::vector<value_type>::iterator<const value_type>			const_iterator;
+		/*
+		** Utiliser les iterateurs customs - implementation defined
+		*/
+		typedef ft::vector_iterator<value_type>								iterator;
+		typedef ft::vector_iterator<const value_type>						const_iterator;
 
-		typedef std::vector<value_type>::reverse_iterator< iterator >		reverse_iterator;
-		typedef std::vector<value_type>::reverse_iterator< const_iterator >	const_reverse_iterator;
-		typedef std::vector<value_type>::ptrdiff_t							difference_type;
-		typedef size_t														size_type;
+		/*
+		** Pour utiliser les iterateurs de la librairie standard
+		*/
+		// typedef std::vector<value_type>::iterator<const value_type>		const_iterator;
+		typedef typename allocator_type::size_type       					size_type;
+		typedef typename allocator_type::difference_type 					difference_type;
+		typedef std::reverse_iterator<iterator>          					reverse_iterator;
+		typedef std::reverse_iterator<const_iterator>    					const_reverse_iterator;
 
-		typedef ft::iterator												iterator; 
+		
+		// typedef ft::reverse_iterator< iterator >			reverse_iterator;
+		// typedef ft::reverse_iterator< const_iterator >	const_reverse_iterator;
+		// typedef std::ptrdiff_t							difference_type;
+		// typedef size_t									size_type;
+
 
 	private:
 		size_type		_size;
 		size_type		_capacity;
 		allocator_type	_allocator;
-		pointer			_data;
+		pointer			_ptr;
 
 		/* Fonctions utiles perso */
 		void	setCapacity(size_type capacity)
