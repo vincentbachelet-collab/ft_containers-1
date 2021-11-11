0d55b7b85a7769f2006b511588bb0048157dc282
Mahaut Latinis <malatini@MacBook-Pro.local>
2021-11-11 18:50:50 +0100

    equal

--- srcs/utils.hpp
+++ srcs/utils.hpp
@@ -4,6 +4,8 @@
 #include <type_traits>
 #include <iterator>
 
+#define DEBUG 1
+
 namespace ft
 {
 	//https://en.cppreference.com/w/cpp/memory/allocator_traits
@@ -147,4 +149,58 @@ namespace ft
 	{
 		typedef T type;
 	};
+
+	/*
+	** Equal
+	** https://en.cppreference.com/w/cpp/algorithm/equal
+	** defined in header <algorithm>
+	*/
+
+	/*
+	** Returns true if the range [first1, last1) is equal to the range 
+	** [first2, first2 + (last1 - first1)), and false otherwise
+	*/
+	template< class InputIt1, class InputIt2 >
+	bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2)
+	{
+		/* Inspiree de la documentation officielle */
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!(*first1 == *first2))
+			{
+				if (DEBUG)
+				{
+					std::cout << "equal algorithm will return false" << std::endl;
+				}
+				return false;
+        	}
+    	}
+		if (DEBUG)
+		{
+			std::cout << "equal algorithm will return true" << std::endl;
+		}
+    	return true;
+	}
+
+	template< class InputIt1, class InputIt2, class BinaryPredicate >
+	bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p)
+	{
+		/* Inspiree de la documentation officielle */
+		for (; first1 != last1; ++first1, ++first2)
+		{
+			if (!p(*first1, *first2))
+			{
+				if (DEBUG)
+				{
+					std::cout << "equal algorithm will return false" << std::endl;
+				}
+				return false;
+			}
+    	}
+	if (DEBUG)
+	{
+		std::cout << "equal algorithm will return true" << std::endl;
+	}
+    return true;
+	}
 }
