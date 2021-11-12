528aa34bebd722023f980c95aca8a89d93c29840
Mahaut Latinis <malatini@MacBook-Pro.local>
2021-11-12 22:55:31 +0100

    ajout fichier checker

---
+++ srcs/checker.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <iterator>
+
+#include "vector.hpp"
+#include "map.hpp"
+#include "map.hpp"
+
+#ifndef NAMESPACE
+# define NAMESPACE ft
+#endif
+
+#ifndef TYPE
+# define TYPE int
+#endif
+
+/*
+** Voir si ca peut vraiment servir au testeur
+*/
