7d1111a8d470da983ddd70687ff527b58b216c70
malatini42 <mahautlatinisheritier@gmail.com>
2022-01-20 10:56:15 +0100

    swap verif ok

--- main.cpp
+++ main.cpp
@@ -1700,15 +1700,12 @@ void my_map_correc(void)
     NS::map<int, int> map1;
     map1.insert(bar);
     NS::map<int, int>::iterator it1 = map1.begin();
-    //std::cout << &(*it1) << std::endl;
     NS::map<int, int> map2;
     map2.insert(foo);
     NS::map<int, int>::iterator it2 = map2.begin();
-    //std::cout << &(*it2) << std::endl;
     NS::swap(map1, map2);
-    //std::cout << &(*it1) << std::endl;
-    //std::cout << &(*it2) << std::endl;
-    //map1.swap(map2);
+    std::cout << (it1 == map2.begin()) << std::endl;
+	std::cout << (it2 == map1.begin()) << std::endl;
     return;
 }
 
